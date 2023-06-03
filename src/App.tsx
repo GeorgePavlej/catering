97c68a06f8cb7d4a66561b6d367b44e52fe11ade
Viktor <vasha2020@gmail.com>
2023-06-03 23:50:55 +0100

    build react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React, { useState } from 'react';
 import { Header } from './Components/Header/Header';
-import { LangContext } from './Components/LangContex/LangContext';
 import './App.css';
 
 function App() {
-  const [lang, setLang] = useState('ua');
-
   return (
     <div className="App">
-      <LangContext.Provider value={lang}>
-        <Header />
-      </LangContext.Provider>
+      <Header />
     </div>
   );
 }
